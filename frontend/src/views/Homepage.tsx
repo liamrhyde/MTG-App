bf018372898f5693018075f3e26786b34d42fa5f
Paskal W <paskal.wolski@gmail.com>
2026-08-13 11:25:37 +0200

    [Frontend] Adds shadcn and tailwind to project
    - Includes barebones button component

--- frontend/src/views/Homepage.tsx
+++ frontend/src/views/Homepage.tsx
@@ -1,12 +1,7 @@
 export const Homepage = () => {
     return (
-        <div style={{ padding: '2rem' }}>
+        <div style={{ padding: "2rem" }}>
             <h1>MTG Tracker</h1>
-            <div style={{ display: 'flex', gap: '1rem', marginTop: '2rem' }}>
-                <button>Start Game</button>
-                <button>Join Game</button>
-                <button>Add Deck</button>
-            </div>
         </div>
     );
 };
