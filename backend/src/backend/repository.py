b084063c9906d670f9e6305d2238f3dbdb2bd4c6
Paskal W <paskal.wolski@gmail.com>
2026-08-27 11:26:16 +0200

    [Backend] Adds repository function to get all players

--- backend/src/backend/repository.py
+++ backend/src/backend/repository.py
@@ -1,8 +1,9 @@
+from sqlmodel import select
 from db.records import Deck
 from typing import Annotated
 
 from db import SessionDep
-from db.records import Player
+from db.records import Player, Game
 from fastapi import Depends
 from sqlmodel import Session
 
@@ -11,11 +12,14 @@ class Repository:
     def __init__(self, session: Session):
         self.session = session
 
+    def commit(self):
+        self.session.commit()
+
     def get_player(self, player_id: int):
         return self.session.get(Player, player_id)
 
-    def commit(self):
-        self.session.commit()
+    def get_players(self):
+        return self.session.exec(select(Player)).all()
 
 
 def get_repository(session: SessionDep):
--- backend/tests/unit/test_repository.py
+++ backend/tests/unit/test_repository.py
@@ -1,13 +1,16 @@
+from sqlmodel import Session
 from backend.repository import Repository, get_repository
 from db.records import Player
 
 
 class TestGetPlayer:
-    def test_returns_existing_player(self, session, repository):
+    def test_returns_existing_player(self, session: Session, repository: Repository):
         player = Player(name="Alice")
         session.add(player)
         session.commit()
+        session.refresh(player)
 
+        assert player.id is not None
         result = repository.get_player(player.id)
 
         assert result is not None
@@ -16,3 +19,23 @@ class TestGetPlayer:
 
     def test_returns_none_for_missing_id(self, repository):
         assert repository.get_player(999) is None
+
+
+class TestGetPlayers:
+    def test_returns_all_players(self, session: Session, repository: Repository):
+        player_1 = Player(name="Alice")
+        player_2 = Player(name="Bob")
+        session.add(player_1)
+        session.add(player_2)
+        session.commit()
+
+        result = repository.get_players()
+        assert result is not None
+        assert len(result) == 2
+        for p in [player_1, player_2]:
+            assert p in result
+            assert p.id is not None
+
+    def test_returns_empty_player_list(self, repository: Repository):
+        result = repository.get_players()
+        assert len(result) == 0
